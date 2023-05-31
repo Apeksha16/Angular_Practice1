accb38d5d271488fedd98d47f7aec0a23348acca
Apeksha16 <apeksha16verma@gmail.com>
2023-05-31 18:08:50 +0530

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice1';
+
+  title = 'UserManagement';
+  isAddPopup: boolean = false;
+  userName: string = "";
+  designation: string = "";
+  mobileNumber: string = "";
+  emailId: string = "";
+  address: string = "";
+  userInfo: any[] = [];
+
+  addUserDetails = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    designation: new FormControl('', Validators.required),
+    mobileNumber: new FormControl('', Validators.required),
+    emailId: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required)
+  });
+
+
+  onAddUser() {
+    this.isAddPopup = !this.isAddPopup;
+  }
+  onSaveUser() {
+    if (this.addUserDetails.valid) {
+      this.userInfo.push(this.addUserDetails.value);
+      this.addUserDetails.reset();
+      this.isAddPopup = false;
+    }
+
+  }
+  onViewUserDetail(userInfo: any) {
+    this.userName = userInfo.userName;
+    this.designation = userInfo.designation;
+    this.mobileNumber = userInfo.mobileNumber;
+    this.emailId = userInfo.emailId;
+    this.address = userInfo.address;
+
+  }
+  clearDetails() {
+    this.userName = "";
+    this.designation = "";
+    this.mobileNumber = "";
+    this.emailId = "";
+    this.address = "";
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -12,7 +23,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatBadgeModule,
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
